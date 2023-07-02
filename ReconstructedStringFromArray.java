6cc2dafaf39755d05b8c4d8537e299a15a6aae12
Melis Ibisheva <93177517+MelisIbisheva@users.noreply.github.com>
2023-07-02 19:12:05 +0200

    Reconstructed string

---
+++ ReconstructedStringFromArray.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ReconstructedStringFromArray {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String[] array= scanner.nextLine().split(", ");
+        String result = reconstructedMethod(array);
+        System.out.println(result);
+    }
+
+    private static String reconstructedMethod(String[] array) {
+        StringBuilder reconstructedString = new StringBuilder();
+        List<String> stringList = new ArrayList<>();
+        String firstString = "";
+        String lastString = "";
+        for (int i = 0; i < array.length; i++) {
+            String currentString = array[i];
+            char firstChar = currentString.charAt(0);
+            char lastChar = currentString.charAt(currentString.length()-1);
+            if(firstChar=='A'){
+                firstString=currentString;
+                reconstructedString.append(firstString);
+            } else if (lastChar=='Z') {
+                lastString = currentString;
+            }else {
+                stringList.add(currentString);
+            }
+
+        }
+        String firsElementInList = stringList.get(0);
+        if(firstString.charAt(firstString.length()-1) !=firsElementInList.charAt(0)){
+            reconstructedString.append(firsElementInList);
+        }else {
+            String stringForAdd=firsElementInList.substring(1);
+        }
+        for (int i = 0; i < stringList.size()-1; i++) {
+            String currentString = stringList.get(i);
+            char lastChar = currentString.charAt(currentString.length()-1);
+            String nextString = stringList.get(i+1);
+            char firstChar = nextString.charAt(0);
+            if(lastChar==firstChar){
+                String stringForAdd = nextString.substring(1);
+                reconstructedString.append(stringForAdd);
+
+            } else {
+                reconstructedString.append(nextString);
+            }
+
+        }
+        reconstructedString.append(lastString);
+        return reconstructedString.toString();
+    }
+}
